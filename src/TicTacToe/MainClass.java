b002e118c23396e9aa0f616087d0a8cceee44831
Bhola Kumar <bholakumar.7372@gmail.com>
2025-06-15 16:21:34 +0530

    Tic Tac Toe

---
+++ src/TicTacToe/MainClass.java
@@ -0,0 +1,10 @@
+package TicTacToe;
+
+public class MainClass {
+    public static void main(String args[]) {
+        TicTacToeGame game = new TicTacToeGame();
+        game.initializeGame();
+        System.out.println("game winner is: " + game.startGame());
+    }
+
+}
